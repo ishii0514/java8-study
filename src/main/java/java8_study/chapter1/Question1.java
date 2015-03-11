e1f9c2afa0f8478a7cff3b7dd70f3be63cce079e
Yosuke Ishii <ishii0514@gmail.com>
2015-03-12 00:08:57 +0900

    initial commit

---
+++ src/main/java/java8_study/chapter1/Question1.java
@@ -0,0 +1,29 @@
+package java8_study.chapter1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class Question1 
+{
+    public static void main( String[] args )
+    {
+    	String[] array = new String[16*1024];
+    	Random random = new Random();
+    	for (int i = 0; i < array.length; i++) {
+    	    array[i] = Double.toString(random.nextDouble());
+    	}
+    	long threadID = Thread.currentThread().getId();
+    	System.out.println("Main Thread is " + threadID);
+    	Arrays.sort(array, (first, second) -> {
+    	    if (Thread.currentThread().getId() != threadID) {
+    	        System.out.println("Comparator Thread is " + Thread.currentThread().getId());
+    	    }
+    	    return first.compareToIgnoreCase(second);
+    	});
+    	//System.out.println(Arrays.toString(array));
+    }
+}
