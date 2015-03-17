393966c5dfa2dffcca768cb6131e3caefd9795ad
ishii <ishii0514@gmail.com>
2015-03-17 21:11:22 +0900

    処理を共通化

--- src/main/java/java8_study/chapter1/Question8.java
+++ src/main/java/java8_study/chapter1/Question8.java
@@ -8,26 +8,31 @@ public class Question8 {
 	public static void main( String[] args )
     {
 		String[] names = {"Peter", "Paul", "Mary"};
+
+		//enhanced for roop
 		List<Runnable> runners = new ArrayList<>();
 		for (String name : names){
 			runners.add(()->System.out.println(name));
 		}
-		for (Runnable runner : runners){
-			//runner.run();
-			new Thread(runner).start();
-		}
-		
+		execute(runners);
+
+		//for roop
 		List<Runnable> runners2 = new ArrayList<>();
 		for (int i=0; i < names.length; i++){
-			//runners2.add(()->System.out.println(names[i]));	//<-Local variable i defined in an enclosing scope must be final or effectively final
+			//compile error: Local variable i defined in an enclosing scope must be final or effectively final
+			//runners2.add(()->System.out.println(names[i]));
+
+			//ok
 			String name = names[i];
-			runners2.add(()->System.out.println(name));	//<-OK
+			runners2.add(()->System.out.println(name));
 		}
-		
-		for (Runnable runner : runners2){
+		execute(runners2);
+
+    }
+
+	private static void execute(List<Runnable> runners){
+		for (Runnable runner : runners){
 			new Thread(runner).start();
 		}
-		
-		
-    }
+	}
 }
