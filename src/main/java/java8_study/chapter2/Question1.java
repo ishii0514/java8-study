9c7b326f5fce16274ee2884ff323e39d58d86d29
Yosuke Ishii <ishii0514@gmail.com>
2015-04-05 01:36:25 +0900

    2章第1問

--- src/main/java/java8_study/chapter2/Question1.java
+++ src/main/java/java8_study/chapter2/Question1.java
@@ -6,22 +6,51 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
-
+//http://closedunbounded.hatenablog.com/entry/2015/01/19/132455
 public class Question1 {
-    public static void main( String[] args ) throws IOException
+	public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException
     {
     	String alice = Question1.class.getClassLoader().getResource("./alice.txt").getFile();
-    	alice = alice.substring(1);
+    	//alice = alice.substring(1);
     	String contents = new String(Files.readAllBytes(Paths.get(alice)), StandardCharsets.UTF_8);
     	List<String> words = Arrays.asList(contents.split("[//P{L}]+"));
-
+    	
+    	
     	int count = 0;
     	for (String w:words){
     		if (w.length() > 12) count++;
     	}
-    	System.out.println("count=" + Integer.toString(count));
+    	System.out.println("no thread count=" + Integer.toString(count));
+    	
+    	
+    	int threadCount = 100;
+    	int wordCount = words.size();
+    	Object[] futures = new Object[wordCount];
+    	ExecutorService exec = Executors.newFixedThreadPool(threadCount);
+    	for (int i=0;i < wordCount;i++){
+    		String w = words.get(i);
+    		futures[i] = exec.submit(() -> {
+    			if(w.length() > 12) return 1;
+    			return 0;
+    		});
+    	}
+    	
+    	AtomicInteger atmicCount = new AtomicInteger();
+    	for (int i=0;i < wordCount;i++){
+    		@SuppressWarnings("unchecked")
+			Future<Integer> future = (Future<Integer>)futures[i];
+    		atmicCount.addAndGet(future.get());
+    	}
+    	System.out.println("thread count=" + atmicCount.toString());
 
     }
+	
+	
 }
 
