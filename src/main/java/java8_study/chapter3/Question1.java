909d04494ef4b3903543161930d8b826d114dc1b
ishii <ishii0514@gmail.com>
2015-04-06 11:01:37 +0900

    変数名修正

---
+++ src/main/java/java8_study/chapter3/Question1.java
@@ -0,0 +1,37 @@
+package java8_study.chapter3;
+
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Question1 {
+	public static void main( String[] args )
+    {
+		int i = 3;
+		String[] a = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l"};
+
+		LoggerEx logger = LoggerEx.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		logger.logif(Level.FINEST,() -> i == 10, () -> "a[10] = " + a[10]);
+
+    }
+
+	public static class LoggerEx{
+		private Logger logger;
+		protected LoggerEx(String name) {
+			logger = Logger.getLogger(name);
+		}
+
+		public static LoggerEx getLogger(String name){
+			return  new LoggerEx(name);
+		}
+
+		public void logif(Level level, Supplier<Boolean> condition, Supplier<String> msgSupplier){
+			if (this.logger.isLoggable(level) && condition.get()){
+				this.logger.log(level,msgSupplier.get());
+			}
+
+		}
+
+	}
+}
