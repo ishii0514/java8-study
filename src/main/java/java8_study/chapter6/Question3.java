fb529678b51d4c276d597edfd876ce406a72b318
Yosuke Ishii <ishii0514@gmail.com>
2015-05-17 16:23:01 +0900

    6章第３問

---
+++ src/main/java/java8_study/chapter6/Question3.java
@@ -0,0 +1,79 @@
+package java8_study.chapter6;
+
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Supplier;
+
+public class Question3 {
+	public static final int Thread_number = 1000;
+	public static final int Count_number = 100000;
+	
+	
+	public static void main(String[] args){
+		System.out.println("-- use AtomicLong --------");
+		AtomicLong counter = new  AtomicLong(0);
+		getPerf(() ->{
+		    	for(int j=0;j<Count_number;j++){
+		    		counter.incrementAndGet();
+		    	}
+		    	return 0L;
+		    },
+		    () -> counter.get()
+		    );
+		System.out.println("-- use LongAdder --------");
+		LongAdder counterAdder = new  LongAdder();
+		getPerf(() ->{
+		    	for(int j=0;j<Count_number;j++){
+		    		counterAdder.increment();
+		    	}
+		    	return 0L;
+		    },
+		    () -> counterAdder.sum()
+		    );
+		
+	}
+	public static void getPerf(Callable<Long> callable,Supplier<Long> fun){
+		ExecutorService pool = Executors.newFixedThreadPool(Thread_number);		
+		List<Future<Long>> list = new ArrayList<>();
+		
+		Instant start1 = Instant.now();	
+		for (int i = 0; i < Thread_number; i++) {
+			Future<Long> future = pool.submit(callable);
+			list.add(future);
+		}
+		
+		Instant start2 = Instant.now();
+		
+		for (Future<Long> future : list) {
+			try {
+				future.get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+		long result = fun.get();
+		Instant end = Instant.now();
+		
+		
+		Duration sumbitTime = Duration.between(start1, start2);
+		Duration executeTime = Duration.between(start2, end);
+		Duration totalTime = Duration.between(start1, end);
+		
+		System.out.println("result:" + Long.toString(result));
+		System.out.println("submit:" + Long.toString(sumbitTime.toMillis()));
+		System.out.println("execute:" + Long.toString(executeTime.toMillis()));
+		System.out.println("total:" + Long.toString(totalTime.toMillis()));
+		
+		pool.shutdown();	
+	}
+}
